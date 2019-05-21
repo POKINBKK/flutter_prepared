855565613ce727b886511bd9d96be150b6aa6bfa
Supakit Theanthunyakit <mr.pokiloli@gmail.com>
2019-05-21 09:21:35 +0700

    Add Color change

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,18 @@ import './ui/friendPage.dart';
 
 void main() => runApp(MyApp());
 
+const PrimaryColor = const Color(0xFFff6337);
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Prepared',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: PrimaryColor,
       ),
       initialRoute: "/",
       routes: {
